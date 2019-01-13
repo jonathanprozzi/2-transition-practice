b106a56c19568064f6dbe2d650e715f4431c7726
jonathanprozzi <jonathanprozzi@gmail.com>
2019-01-12 21:11:01 -0500

    getting closer: animation now contained in grids, but still having issue with it starting at 50% height

--- src/App.js
+++ src/App.js
@@ -1,55 +1,87 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 import { config } from "react-spring";
-import { Spring, transition, animated } from "react-spring";
+import { Spring, Transition, animated } from "react-spring";
+
+const pages = [
+  style => (
+    <animated.div style={{ ...style, background: "#b3FFBD" }}>
+      <SectionTitle>One: 🧙</SectionTitle>
+    </animated.div>
+  ),
+  style => (
+    <animated.div style={{ ...style, background: "#B2DBBF" }}>
+      {" "}
+      <SectionTitle>Two: 🧙</SectionTitle>
+    </animated.div>
+  ),
+  style => (
+    <animated.div style={{ ...style, background: "#12DBBF" }}>
+      {" "}
+      <SectionTitle>Three: 🧙</SectionTitle>
+    </animated.div>
+  )
+];
 
 class App extends Component {
+  state = { index: 0 };
+  toggle = e =>
+    this.setState(state => ({
+      index: state.index === 2 ? 0 : state.index + 1
+    }));
   render() {
     return (
       <AppWrapper>
-            <SectionContainer bgColor="#16c79e">
-              <SectionTitle>Section One</SectionTitle>
-              <GhostButton>
-                Interesting... 🧐
-              </GhostButton>
-            </SectionContainer>
+        <SectionContainer bgColor="#16c79e">
+          <SectionTitle>Section One</SectionTitle>
+          <GhostButton>Interesting... 🧐</GhostButton>
+        </SectionContainer>
 
-            <SectionContainer bgColor="#109173">
-              <SectionTitle>Section Two</SectionTitle>
-              <GhostButton>
-              Interesting... 🧐
-              </GhostButton>
+        <SectionContainerGrid bgColor="#109173">
+          <SubSectionContainer>
+            <SubSection bgColor="#32a98d">
+              <SectionTitle>...and then? 🦄</SectionTitle>
+            </SubSection>
+            <SubSection bgColor="#5ecfb5">
+              <SectionTitle>...and then? 🦄</SectionTitle>
+            </SubSection>
+            <SubSection bgColor="#8dddca">
+              <SectionTitle>...and then? 🦄</SectionTitle>
+            </SubSection>
+          </SubSectionContainer>
+        </SectionContainerGrid>
 
-                  <SubSectionContainer>
-                    <SectionTitle>Sub 1</SectionTitle>
-                    <GhostButton>
-                      ...and then? 🦄
-                    </GhostButton>
-                  </SubSectionContainer>
-
-                  <SubSectionContainer>
-                    <SectionTitle>Sub 2</SectionTitle>
-                    <GhostButton>
-                      ...and then? 🦄
-                    </GhostButton>
-                  </SubSectionContainer>
-
-                  <SubSectionContainer>
-                    <SectionTitle>Sub 3</SectionTitle>
-                    <GhostButton>
-                      ...and then? 🦄
-                    </GhostButton>
-                  </SubSectionContainer>
-
-            </SectionContainer>
-
-          <ParallaxLayer offset={2}>
-            <SectionContainer bgColor="#0a5c49">
-              <SectionTitle>Section Three</SectionTitle>
-              <GhostButton>
-              Interesting... 🧐
-              </GhostButton>
-            </SectionContainer>
+        <SectionContainer bgColor="#0a5c49">
+          <SectionTitle>Section Three</SectionTitle>
+          <GhostButton>Interesting... 🧐</GhostButton>
+        </SectionContainer>
+        <SectionContainerGridTwo onClick={this.toggle}>
+          <SubSection>
+            <SectionTitle>Hi</SectionTitle>
+          </SubSection>
+          <SubSection>
+            <Transition
+              native
+              unique
+              reset
+              items={this.state.index}
+              from={{
+                opacity: 0,
+                transform: "translate3d(100%,0%,0)"
+              }}
+              enter={{
+                opacity: 1,
+                transform: "translate3d(0%,0%,0)"
+              }}
+              leave={{
+                opacity: 0,
+                transform: "translate3d(-100%,0%,0)"
+              }}
+            >
+              {index => pages[index]}
+            </Transition>
+          </SubSection>
+        </SectionContainerGridTwo>
       </AppWrapper>
     );
   }
@@ -80,7 +112,41 @@ const SectionContainer = styled.div`
   justify-content: center;
 `;
 
+const SectionContainerGrid = styled.div`
+  background: ${props => (props.bgColor ? props.bgColor : "#2d2d2d")};
+  height: 100%;
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr 1fr;
+`;
+
+const SectionContainerGridTwo = styled.div`
+  background: ${props => (props.bgColor ? props.bgColor : "#2d2d2d")};
+  height: 100%;
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+
+  div {
+    will-change: transform, opacity;
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
 const SubSectionContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+const SubSection = styled.div`
   background: ${props => (props.bgColor ? props.bgColor : "#2d2d2d")};
   height: 100%;
   width: 100%;
